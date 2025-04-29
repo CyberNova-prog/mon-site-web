d0ec6726e38316756fc9622c2c268a9f04fe3ba3
CyberNova-prog <Safsafkny@gmail.com>
2025-04-29 16:12:32 +0200

    Create script.js
    Ajout du fichier JavaScript pour l'interactivité

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ajouter un événement sur le bouton
+document.getElementById("myButton").addEventListener("click", function() {
+    // Change le texte du bouton quand on clique dessus
+    this.innerHTML = "Vous avez cliqué !";
+});
